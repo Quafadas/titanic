42938dc2e4790b764abacc0a230f91f225da2c84
Simon Parten <quafadas@gmail.com>
2025-01-02 22:06:31 +0100

    .

--- analysis.scala
+++ analysis.scala
@@ -1,6 +1,9 @@
 //> using scala 3.6.2
 
-//> using dep io.github.quafadas::scautable::0.0.11-15-4fee12-DIRTYf22e85e5
+//> using resourceDir data
+
+//> using dep io.github.quafadas::scautable::0.0.11-23-3f71ff-DIRTY144c070
+
 //> using options -experimental -language:experimental.namedTuples
 
 import io.github.quafadas.scautable.scautable.*
@@ -8,34 +11,37 @@ import io.github.quafadas.scautable.CSV
 import io.github.quafadas.scautable.CSV.*
 import NamedTuple.*
 
+enum Gender:
+  case Male, Female
+
 @main def titanic =
 
-  def csv = CSV.absolutePath("/Users/simon/Code/titanic/titanic.csv")
+  def csv = CSV.resource("titanic.csv")
 
-  def csvDropHeader = csv.drop(1)
+  def data = csv
+    .mapColumn["Sex", Gender]((x: String) => Gender.valueOf(x.capitalize))
+    .dropColumn["PassengerId"]
 
-  val data = csvDropHeader
-    .addColumn["SurvivedB", Boolean](_.Survived == "1")
-
-  val survived = csvDropHeader
+  val survived = data
     .column["Survived"]
     .toArray
     .groupMapReduce(identity)(_ => 1)(_ + _)
 
-  val sex = csvDropHeader
+  val sex = data
     .column["Sex"]
     .toArray
     .groupMapReduce(identity)(_ => 1)(_ + _)
     .toList
 
-  val age = csvDropHeader
-    .column["Age", Option[Double]](_.toDoubleOption)
+  val age = csv
+    .mapColumn["Age", Option[Double]](_.toDoubleOption)
+    .column["Age"]
     .toArray
     .groupMapReduce(identity)(_ => 1)(_ + _)
     .toList
 
   val group =
-    csvDropHeader
+    csv.toArray
       .map(x => (x.Survived, x.Sex).withNames[("Survived", "Sex")])
       .toArray
       .groupMapReduce(_.Sex)(x => (x.Survived.toInt, 1, 0.0)) {
@@ -54,7 +60,7 @@ import NamedTuple.*
   println(data.toArray.take(20).consolePrint())
 
   println(
-    s"survived $survived : % of total ${survived.get("1").sum.toDouble / csvDropHeader.size.toDouble}"
+    s"survived $survived : % of total ${survived.get("1").sum.toDouble / csv.size.toDouble}"
   )
   println(sex.consoleShow)
 
