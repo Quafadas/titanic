1e649953726f5ef042324d17d2ee202130fa3163
Simon Parten <quafadas@gmail.com>
2025-01-05 21:01:30 +0100

    .

--- analysis.scala
+++ analysis.scala
@@ -2,12 +2,16 @@
 
 //> using resourceDir data
 
-//> using dep io.github.quafadas::scautable:0.0.14-3-3ca125
+//> using dep io.github.quafadas::scautable:0.0.14-5-96e496
+
+
 
 //> using options -experimental -language:experimental.namedTuples
 
 import io.github.quafadas.table.*
 import NamedTuple.*
+import io.github.quafadas.scautable.ConsoleFormat
+import scala.util.Try
 
 enum Gender :
   case Male, Female
@@ -26,12 +30,18 @@ enum Gender :
     .dropColumn["PassengerId"]
     .mapColumn["Age", Option[Double]](_.toDoubleOption)
     .mapColumn["Survived", Boolean](_ == "1")
+    .mapColumn["Pclass", Int](_.toInt)
+    .mapColumn["SibSp", Int](_.toInt)
+    .mapColumn["Parch", Int](_.toInt)
+    .mapColumn["Fare", Double](_.toDouble)
+
 
 
   def survivedCol = data.column["Survived"]
-  def surived= survivedCol.foldLeft((0, 0, 0.0)){case (acc, survived) =>
+  def surived= survivedCol.foldLeft((0, 0, "")){case (acc, survived) =>
     val survivedI = if survived then 1 else 0
-    (acc._1 + survivedI, acc._2 + 1, 100 * acc._1.toDouble / acc._2.toDouble)
+    (acc._1 + survivedI, acc._2 + 1, ConsoleFormat.formatAsPercentage((acc._1 + survivedI).toDouble / (acc._2 + 1).toDouble))
+
   }.withNames[("Survived", "Total", "%")]
 
   val dataArr = data.toArray
@@ -45,12 +55,12 @@ enum Gender :
   val group =
     dataArr
       .map(x => (x.Survived, x.Sex).withNames[("Survived", "Sex")])
-      .groupMapReduce(_.Sex)(x => (if(x.Survived) 1 else 0, 1, 0.0)) {
+      .groupMapReduce(_.Sex)(x => (if(x.Survived) 1 else 0, 1, "")) {
         case ((surviveAcc, oneAcc, percAcc), (c, d, e)) =>
           (
             surviveAcc + c,
             oneAcc + d,
-            100 * (surviveAcc + c).toDouble / (oneAcc + d).toDouble
+            ConsoleFormat.formatAsPercentage( (surviveAcc + c).toDouble / (oneAcc + d).toDouble)
           )
       }
       .toList
